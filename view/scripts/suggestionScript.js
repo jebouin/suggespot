31d2268fe0c430bd8633dad289c995ba38deef3c
jebouin <jebouin@gmail.com>
2017-03-07 11:15:45 +0100

    focus textarea on reply

--- view/scripts/suggestionScript.js
+++ view/scripts/suggestionScript.js
@@ -243,7 +243,7 @@ $(document).ready(function() {
 	//comment
 	$(document).keydown(function(e) {
   		if(e.which === 13 && e.ctrlKey) {
-    		$("input[type='submit'").click();
+    		$("input[type='submit']:visible").last().click();
     	}
 	});
 
@@ -287,13 +287,14 @@ $(document).ready(function() {
 });
 
 function reply(event) {
-	$(event.target).css("display", "none");
+    $(event.target).css("display", "none");
 	var replies = $(event.target).parent();
 	var commentUI = $(".commentUI");
 	commentUI.css("display", "block");
-	commentUI.detach().appendTo(replies);
+    commentUI.detach().appendTo(replies);
 	var cid = replies.attr("cid");
 	$("input[name='parent']", commentUI).val(cid);
+    $(".commentUI textarea").focus();
 }
 
 function cancelComment(event) {
